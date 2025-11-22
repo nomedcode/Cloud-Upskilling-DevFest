e35d7c68aa4e33b7001b74f0d5542fce7c0163db
nomedcode <faiqhiadraprasetia@gmail.com>
2025-11-22 18:12:24 +0000

    commit

--- config.php
+++ config.php
@@ -5,6 +5,10 @@ $db_name = 'image_catalog';
 $db_user = 'appmod-phpapp-user';
 $db_pass = 'lego123jawa';
 
+echo "<h1>DEBUG CHECK</h1>";
+echo "The Container is using Password: [" . $db_pass . "]";
+die();
+
 try {
     $pdo = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
