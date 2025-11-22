6c17b0ced112ed9d6f51f2c8c987090a77b2829d
nomedcode <faiqhaidarprasetya0@gmail.com>
2025-11-22 11:10:15 +0000

    commit

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 // Configurazione del database
-$db_host = 'localhost';
+$db_host = '34.50.111.51';
 $db_name = 'image_catalog';
-$db_user = 'root';
-$db_pass = 'veryverystrongpassword';
+$db_user = 'appmod-phpapp-user';
+$db_pass = 'Lego123jawa';
 
 try {
     $pdo = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass);
