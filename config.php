79651b59ef524fdb213b123bc3082af15d2b1c39
nomedcode <faiqhiadraprasetia@gmail.com>
2025-11-22 18:42:41 +0000

    commit

--- config.php
+++ config.php
@@ -1,13 +1,8 @@
 <?php
-// Configurazione del database
-$db_host = '34.154.150.50';
+$db_host = '34.154.150.50'; // Your Public IP
 $db_name = 'image_catalog';
 $db_user = 'appmod-phpapp-user';
 $db_pass = 'lego123jawa';
-
-echo "<h1>DEBUG CHECK</h1>";
-echo "The Container is using Password: [" . $db_pass . "]";
-die();
 
 try {
     $pdo = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass);
@@ -15,6 +10,5 @@ try {
 } catch (PDOException $e) {
     die("Errore di connessione: " . $e->getMessage());
 }
-
 session_start();
 ?>
